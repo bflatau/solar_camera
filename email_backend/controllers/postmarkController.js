84c582a5b225bc74ea27cfacad86fbacc933787b
Ben <bflatau@gmail.com>
2023-11-09 20:43:38 -0800

    got promises kinda working..

--- email_backend/controllers/postmarkController.js
+++ email_backend/controllers/postmarkController.js
@@ -1,27 +1,112 @@
 const postmark = require("postmark");
 const fs = require('fs');
+var Airtable = require('airtable');
+const axios = require('axios');
+
+
+var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+
+
+
+function fetchAirtableRecords(){
+
+    return new Promise((resolve, reject) => {
+
+        let sunsetImages;
+  
+        base('Table 1').select({
+            // Selecting the first 5 records in Grid view:
+            maxRecords: 5,
+            view: "Grid view"
+        }).eachPage(function page(records, fetchNextPage) {
+            // This function (`page`) will get called for each page of records.
+
+            sunsetImages = records.map(function (record) {
+
+
+                return new Promise((resolve, reject) => {
+                    axios.get(`${record.get('Image_URL')}.jpg`, { responseType: 'arraybuffer' })
+                        .then(response => {
+                           let base64Image = `data:${response.headers['content-type']};base64,` + Buffer.from(response.data).toString('base64');
+                           resolve(base64Image);
+                            // console.log('<img src="' + base64Image + '"/>');
+                        })
+                        .catch(error => console.log(error));    
+                })
+
+                // return record.get('Image_URL')
+            })
+
+            // records.forEach(function (record) {
+            //     console.log('Retrieved', record.get('Image_URL'));
+            //     console.log('Retrieved', record.get('Created'));
+            // });
+
+            // To fetch the next page of records, call `fetchNextPage`.
+            // If there are more records, `page` will get called again.
+            // If there are no more records, `done` will get called.
+            fetchNextPage();
+
+        }, function done(err) {
+            // console.log(sunsetImages)
+            if (err) { console.error(err); return; }
+
+
+            Promise.all(sunsetImages).then(function (results) {
+                // console.log(results)
+                // sunsetImages = results;
+                resolve(results)
+            })
+
+
+
+        });
+
+    });
+
+
+}
+
 
 
 /// SETUP POSTMARK ///
 const client = new postmark.ServerClient(process.env.POSTMARK_API_KEY);
 
-exports.testEmail = (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    client.sendEmail({
-        "From": process.env.FROM_EMAIL,
-        "To": process.env.TO_EMAIL,
-        "Subject": `Today's Santa Cruz Sunset`,
-        // "Tag": req.body.type,
-        "HtmlBody": fs.readFileSync("./templates/test-email.html", "utf8"),
-        "TrackOpens": true,
-        "TrackLinks": "HtmlAndText",
-        "MessageStream": "outbound"
-    });
+async function testEmail (req, res){
+
+
+    let imageArray = await fetchAirtableRecords();
+
+    console.log('this is image array', imageArray)
+
+
+    // res.header("Access-Control-Allow-Origin", "*");
+    // client.sendEmail({
+    //     "From": process.env.FROM_EMAIL,
+    //     "To": process.env.TO_EMAIL,
+    //     "Subject": `Today's Santa Cruz Sunset`,
+    //     // "Tag": req.body.type,
+    //     "HtmlBody": fs.readFileSync("./templates/test-email.html", "utf8"),
+    //     "TrackOpens": true,
+    //     "TrackLinks": "HtmlAndText",
+    //     "MessageStream": "outbound",
+    //     // "Attachments":[
+
+    //     //     {
+    //     //         "Name": "image-1.jpg",
+    //     //         "ContentID": "cid:image-1.jpg",
+    //     //         "Content": 'https://res.cloudinary.com/dubqxoatm/image/upload/image_11_09_2023_17:02:53.jpg'.toString('base64'),
+    //     //         "ContentType": "image/jpg"
+    //     //     },
+
+    //     // ]
+
+    // });
     res.json(req.body.email);
-    console.log(req.body)
+    // console.log(req.body)
 }
 
-
+// https://res.cloudinary.com/dubqxoatm/image/upload/image_11_09_2023_17:02:53.jpg
 
 exports.sendEmail = (req, res) => {
 
@@ -96,3 +181,11 @@ exports.sendEmail = (req, res) => {
     res.json(req.body.email);
     console.log(req.body)
 };
+
+
+module.exports.testEmail = testEmail;
+
+
+
+
+
